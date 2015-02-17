bdb245e020f7b1b4d47ed3e3afb6ce7ba78a9be0
Akagi201 <akagi201@gmail.com>
2015-02-17 22:33:52 +0800

    add hello world

---
+++ golang-book/chapter2/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// This is a comment
+
+func main() {
+	fmt.Println("Hello World!")
+	fmt.Println("Hello World!"[1])
+	fmt.Println("Hello " + "World!")
+	fmt.Println(!true)
+	fmt.Println(true && false)
+	fmt.Println(1 * 2)
+}
