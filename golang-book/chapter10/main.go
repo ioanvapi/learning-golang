20ee0f289aca9aba17ca28a4a8d24971b48af01c
Akagi201 <akagi201@gmail.com>
2015-02-23 22:51:47 +0800

    update to chapter10 concurrency

---
+++ golang-book/chapter10/main.go
@@ -0,0 +1,106 @@
+package main
+
+/* goroutine
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func f(n int) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(n, ":", i)
+		amt := time.Duration(rand.Intn(250))
+		time.Sleep(time.Millisecond * amt)
+	}
+}
+
+func main() {
+	//go f(0)
+	for i := 0; i < 10; i++ {
+		go f(i)
+	}
+	var input string
+	fmt.Scanln(&input)
+}
+*/
+
+/* channel
+import (
+	"fmt"
+	"time"
+)
+
+func pinger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "ping"
+	}
+}
+
+func ponger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "pong"
+	}
+}
+
+func printer(c chan string) {
+	for {
+		msg := <-c
+		fmt.Println(msg)
+		time.Sleep(time.Second * 1)
+	}
+}
+
+func main() {
+	var c chan string = make(chan string)
+
+	go pinger(c)
+	go ponger(c)
+	go printer(c)
+
+	var input string
+	fmt.Scanln(&input)
+}
+*/
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		for {
+			c1 <- "from 1"
+			time.Sleep(time.Second * 2)
+		}
+	}()
+
+	go func() {
+		for {
+			c2 <- "from 2"
+			time.Sleep(time.Second * 3)
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case msg1 := <-c1:
+				fmt.Println(msg1)
+			case msg2 := <-c2:
+				fmt.Println(msg2)
+			case <-time.After(time.Second):
+				fmt.Println("timeout")
+			default:
+				fmt.Println("nothing ready")
+			}
+		}
+	}()
+
+	var input string
+	fmt.Scanln(&input)
+}
