3fd67b9269ba84081f64b853c55a56e4f90d947d
Akagi201 <akagi201@gmail.com>
2016-02-02 20:01:00 +0800

    testing: add some tests

---
+++ misc/src/testing/testtrace.go
@@ -0,0 +1,24 @@
+// go run -race testtrace.go
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var i int = 0
+	go func() {
+		for {
+			i++
+			fmt.Println("subroutine: i = ", i)
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	for {
+		i++
+		fmt.Println("mainroutine: i = ", i)
+		time.Sleep(1 * time.Second)
+	}
+}
