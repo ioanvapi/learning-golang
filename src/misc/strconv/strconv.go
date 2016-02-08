3bfb292a05759d0dd7b84f4d2b9f2bb739b55cfb
Akagi201 <akagi201@gmail.com>
2016-02-08 09:52:05 +0800

    misc: add strconv

---
+++ src/misc/strconv/strconv.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	// 基本数值转换
+	fmt.Println(strconv.Itoa(10))
+	fmt.Println(strconv.Atoi("711"))
+
+	// 解析
+	fmt.Println(strconv.ParseBool("false"))
+	fmt.Println(strconv.ParseBool("False"))
+	fmt.Println(strconv.ParseBool("FALSE"))
+	fmt.Println(strconv.ParseFloat("3.14", 32))
+
+	// 格式化
+	fmt.Println(strconv.FormatBool(true))
+	fmt.Println(strconv.FormatInt(123, 10))
+}
