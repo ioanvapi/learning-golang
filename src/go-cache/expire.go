652f9ddad48c5d4b4de51739c3f0fc7905fc7923
Akagi201 <akagi201@gmail.com>
2016-03-04 14:59:41 +0800

    add go-cache

---
+++ src/go-cache/expire.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/patrickmn/go-cache"
+)
+
+func main() {
+
+	// Create a cache with a default expiration time of 5 minutes, and which
+	// purges expired items every 30 seconds
+	c := cache.New(3 * time.Second, 2 * time.Second)
+
+	// Set the value of the key "foo" to "bar", with the default expiration time
+	c.Set("foo", "bar", cache.DefaultExpiration)
+
+	// Get the string associated with the key "foo" from the cache
+	foo, found := c.Get("foo")
+	if found {
+		fmt.Println(foo)
+	}
+
+	time.Sleep(1 * time.Second)
+
+	foo, found = c.Get("foo")
+	if found {
+		fmt.Println(foo)
+	}
+
+	time.Sleep(1 * time.Second)
+
+	foo, found = c.Get("foo")
+	if found {
+		fmt.Println(foo)
+	}
+
+	time.Sleep(1 * time.Second)
+
+	foo, found = c.Get("foo")
+	if found {
+		fmt.Println(foo)
+	}
+
+	time.Sleep(1 * time.Second)
+
+	foo, found = c.Get("foo")
+	if found {
+		fmt.Println(foo)
+	}
+}
