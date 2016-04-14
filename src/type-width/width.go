385e745b28380329e90e9d3ff93f4a4167a98df0
Akagi201 <akagi201@gmail.com>
2016-04-14 14:47:55 +0800

    update type width

--- src/type-width/width.go
+++ src/type-width/width.go
@@ -13,4 +13,28 @@ func main() {
 
 	var a [3]uint32
 	fmt.Println(unsafe.Sizeof(a))
+
+	type SS struct {
+		a uint16
+		b uint32
+	}
+	var ss SS
+	fmt.Println(unsafe.Sizeof(ss))
+
+	var sss struct{}
+	fmt.Println(unsafe.Sizeof(sss))
+
+	type SSSS struct {
+		A struct{}
+		B struct{}
+	}
+	var ssss SSSS
+	fmt.Println(unsafe.Sizeof(ssss))
+
+	var x [1000000000]struct{}
+	fmt.Println(unsafe.Sizeof(x)) // prints 0
+
+	// Slices of struct{}s consume only the space for their slice header
+	var xx = make([]struct{}, 1000000000)
+	fmt.Println(unsafe.Sizeof(xx))
 }
