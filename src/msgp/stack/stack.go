7e43eea35ac25e85d749b87a11e48435dae55ea7
Akagi201 <akagi201@gmail.com>
2016-05-06 17:24:34 +0800

    msgp: add stack

---
+++ src/msgp/stack/stack.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	"github.com/tinylib/msgp/msgp"
+)
+
+//go:generate msgp
+
+//msgp:tuple Primitive
+
+type Primitive struct {
+	One   int
+	Two   uint
+	Three float64
+}
+
+func main() {
+	p := Primitive{1, 2, 3.0}
+	out, _ := p.MarshalMsg(nil)
+	msgp.UnmarshalAsJSON(os.Stdout, out)
+	print("\n")
+}
