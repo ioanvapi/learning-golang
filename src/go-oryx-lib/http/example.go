0b51854bf4a1372fc4c954d00b8628a9991720e6
Akagi201 <akagi201@gmail.com>
2016-05-16 17:58:51 +0800

    go-oryx-lib: update http example

--- src/go-oryx-lib/http/example.go
+++ src/go-oryx-lib/http/example.go
@@ -7,11 +7,45 @@ import (
 	ohttp "github.com/ossrs/go-oryx-lib/http"
 )
 
+type Payload struct {
+	Foo string
+	Bar string
+}
+
+const (
+	errorSystemError ohttp.SystemError = 100 + iota
+	errorSystemComplexError
+)
+
 func main() {
 	ohttp.Server = "akserver"
+
+	// data string
 	data := "data string"
-	fn := ohttp.Data(nil, data).(http.HandlerFunc)
-	http.HandleFunc("/data_string", fn)
+
+	// this is OK
+	// fn := ohttp.Data(nil, data).(http.HandlerFunc)
+	// http.HandleFunc("/data_string", fn)
+
+	http.HandleFunc("/", ohttp.Data(nil, data).ServeHTTP)
+
+	// data struct
+	data1 := Payload{
+		Foo: "foo",
+		Bar: "bar",
+	}
+	http.HandleFunc("/data_struct", ohttp.Data(nil, data1).ServeHTTP)
+
+	// SystemError
+	http.HandleFunc("/system_error", ohttp.Error(nil, errorSystemError).ServeHTTP)
+
+	// SystemComplexError
+	sce := ohttp.SystemComplexError{
+		Code: errorSystemComplexError,
+		Message: "SystemComplexError string",
+	}
+	http.HandleFunc("/system_complex_error", ohttp.Error(nil, sce).ServeHTTP)
+
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
