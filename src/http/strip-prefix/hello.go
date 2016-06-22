1a502b6d1f2518c09314d1796824155d30a47ed4
Akagi201 <akagi201@gmail.com>
2016-06-22 13:55:14 +0800

    add strip prefix

---
+++ src/http/strip-prefix/hello.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// To serve a directory on disk (/tmp) under an alternate URL
+	// path (/tmpfiles/), use StripPrefix to modify the request
+	// URL's path before the FileServer sees it:
+	http.Handle("/tmpfiles/", http.StripPrefix("/tmpfiles/", http.FileServer(http.Dir("/tmp"))))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
