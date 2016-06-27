08859c90ba15ac1dd5fdfa6ea4b2766436d627f3
Akagi201 <akagi201@gmail.com>
2016-06-27 14:46:17 +0800

    add testing example

---
+++ src/misc/testing/example/sum_test.go
@@ -0,0 +1,31 @@
+package example
+
+import (
+	"testing"
+)
+
+type sumTest struct {
+	in1, in2, out int
+}
+
+// This is data provider
+var sumTests = []sumTest{
+	sumTest{1, 2, 3},
+	sumTest{5, 2, 7},
+}
+
+func TestSum(t *testing.T) {
+	for _, test := range sumTests {
+		out := Sum(test.in1, test.in2)
+		if out != test.out {
+			t.Errorf("Sum(%v, %v) = %v, want %v", test.in1, test.in2, out, test.out)
+		}
+	}
+}
+
+// You can run this benchmark using "go test -test.bench Sum"
+func BenchmarkSum(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sum(1, 3)
+	}
+}
