dff52460e5690fbfba5eba4a9644a8fc218272ed
Akagi201 <akagi201@gmail.com>
2016-08-04 21:02:27 +0800

    add ctxhttp

---
+++ src/ctxhttp/timeout.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"time"
+
+	"github.com/Jeffail/gabs"
+	"golang.org/x/net/context"
+	"golang.org/x/net/context/ctxhttp"
+)
+
+func main() {
+	ctx, _ := context.WithTimeout(context.Background(), 3*time.Second)
+
+	resp, err := ctxhttp.Get(ctx, nil, "http://httpbin.org/get")
+	if err != nil {
+		panic(err)
+	}
+
+	resBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	jsonParsed, err := gabs.ParseJSON(resBody)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(jsonParsed.StringIndent("", "  "))
+}
