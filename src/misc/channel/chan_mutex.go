2e5ed6bf01f95ebf0988eef2b6cae8330676cf5f
Akagi201 <akagi201@gmail.com>
2016-09-30 09:36:42 +0800

    chan: add mutex

---
+++ src/misc/channel/chan_mutex.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"time"
+)
+
+func main() {
+	global := 0
+	var lock = make(chan bool, 1)
+
+	lock <- true
+	go func() {
+		<-lock
+		global = 1
+		println(global)
+		lock <- true
+	}()
+
+	go func() {
+		<-lock
+		global = 2
+		println(global)
+		lock <- true
+	}()
+
+	for {
+		time.Sleep(1 * time.Second)
+	}
+}
