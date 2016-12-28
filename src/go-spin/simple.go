ba4959afba8c04ebc1349f95221a81ae9d52733d
Akagi201 <akagi201@gmail.com>
2016-12-28 14:39:30 +0800

    feat(go-spin): add simple

---
+++ src/go-spin/simple.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/tj/go-spin"
+	"fmt"
+	"time"
+)
+
+func main() {
+	s := spin.New()
+	for i := 0; i < 30; i++ {
+		fmt.Printf("\r  \033[36mcomputing\033[m %s ", s.Next())
+		time.Sleep(100 * time.Millisecond)
+	}
+	fmt.Println()
+}
