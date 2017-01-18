bbdaa332a95b5a4d1c07f06536ac1ec642f29f11
Akagi201 <akagi201@gmail.com>
2017-01-18 06:47:27 +0000

    feat(swig): add simple

---
+++ src/swig/simple/README.md
@@ -0,0 +1,4 @@
+## Build
+* `swig -go -cgo -c++ -intgosize 64 simple.i`
+* `go build main.go`
+
---
+++ src/swig/simple/main.go
@@ -0,0 +1,27 @@
+
+package main
+
+import (
+    "./simple"
+    "fmt"
+)
+
+func main() {
+    // Call our gcd() function
+    x := 42
+    y := 105
+    g := simple.Gcd(x, y)
+    fmt.Println("The gcd of", x, "and", y, "is", g)
+
+    // Manipulate the Foo global variable
+
+    // Output its current value
+    fmt.Println("Foo =", simple.GetFoo())
+
+    // Change its value
+    simple.SetFoo(3.1415926)
+
+    // See if the change took effect
+    fmt.Println("Foo =", simple.GetFoo())
+}
+
