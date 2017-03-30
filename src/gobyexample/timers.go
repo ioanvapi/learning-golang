7ec81e1ad0572fded93936606f9d4a0acb1636f0
Akagi201 <akagi201@gmail.com>
2017-03-30 16:17:13 +0800

    feat(gobyexample): update timers.go

--- src/gobyexample/timers.go
+++ src/gobyexample/timers.go
@@ -5,8 +5,11 @@ import "fmt"
 
 func main() {
 	timer1 := time.NewTimer(time.Second * 2)
-	<-timer1.C
+	now := <-timer1.C
+	now1 := time.Now()
+	fmt.Printf("now: %v vs %v\n", now, now1)
 	fmt.Println("Timer 1 expired")
+
 	timer2 := time.NewTimer(time.Second)
 	go func() {
 		<-timer2.C
