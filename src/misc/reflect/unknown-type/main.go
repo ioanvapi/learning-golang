00815ddb231e0847cf4c795f3a749634c940368f
Akagi201 <akagi201@gmail.com>
2018-06-12 11:44:20 +0800

    feat(reflect): add some practices

---
+++ src/misc/reflect/unknown-type/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type User struct {
+	Id   int
+	Name string
+	Age  int
+}
+
+func (u User) ReflectCallFunc() {
+	fmt.Println("Allen.Wu ReflectCallFunc")
+}
+
+func main() {
+	user := User{1, "Allen.Wu", 25}
+
+	DoFiledAndMethod(user)
+}
+
+// 通过接口来获取任意参数，然后一一揭晓
+func DoFiledAndMethod(input interface{}) {
+
+	getType := reflect.TypeOf(input)
+	fmt.Println("get Type is :", getType.Name())
+
+	getValue := reflect.ValueOf(input)
+	fmt.Println("get all Fields is:", getValue)
+
+	// 获取方法字段
+	// 1. 先获取 interface 的 reflect.Type, 然后通过 NumField 进行遍历
+	// 2. 再通过 reflect.Type 的 Field 获取其 Field
+	// 3. 最后通过 Field 的 Interface() 得到对应的 value
+	for i := 0; i < getType.NumField(); i++ {
+		field := getType.Field(i)
+		value := getValue.Field(i).Interface()
+		fmt.Printf("%s: %v = %v\n", field.Name, field.Type, value)
+	}
+
+	// 获取方法
+	// 1. 先获取 interface 的 reflect.Type, 然后通过 .NumMethod 进行遍历
+	for i := 0; i < getType.NumMethod(); i++ {
+		m := getType.Method(i)
+		fmt.Printf("%s: %v\n", m.Name, m.Type)
+	}
+}
